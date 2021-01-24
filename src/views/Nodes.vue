2f653fb4f1fa4a83740d0a2c0ad098b1f6068498
Homin Lee <homin.lee@suapapa.net>
2021-01-24 21:52:46 +0900

    fuzzy search

--- src/views/Nodes.vue
+++ src/views/Nodes.vue
@@ -1,25 +1,37 @@
 <template>
-<input type="text" />
-<div v-if="nodes !== null">
+<input type="text" @keyup="fuzzy_search" :placeholder="ptn" />
+<div v-if="filtered_nodes !== null">
     <ul id="array-rendering">
-        <li v-for="item in nodes" :key="item.node">
-            from {{ item.module }} import {{ item.node }}
+        <li v-for="item in filtered_nodes" :key="item.refIndex">
+            from {{ item.item.module }} import {{ item.item.node }}
         </li>
     </ul>
 </div>
 </template>
 
 <script>
+import Fuse from 'fuse.js'
+
 export default {
   data: () => ({
-      nodes: null,
+      fuse: null,
+      ptn: "",
       filtered_nodes: null
   }),
   mounted(){
     fetch('http://172.30.5.213:8888/nodes')
         .then(res => res.json())
-        .then(data => this.nodes = data)
+        .then(data => {
+            this.fuse = new Fuse(data, {keys: ["module", "node"]})
+        })
+  },
+  methods: {
+    fuzzy_search(e) {
+        this.ptn = e.target.value
+        console.log("fuzzy serch", this.ptn)
+        this.filtered_nodes = this.fuse.search(this.ptn)
     }
+  }
 }
 </script>
 
