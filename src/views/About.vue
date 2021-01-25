171869f478d89b2a18d8a89ae5346f7578dc9327
Homin Lee <homin.lee@suapapa.net>
2021-01-25 18:20:21 +0900

    update About view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p>
+    <b>Diagrams Sandbox </b>is web-based application which 
+    can draw cloud system architecture with 
+    <b><a href="https://diagrams.mingrammer.com/">Diagrams</a></b>
+    script.
+    </p>
+
+    <p>
+      This Appication consits of thee part which is written in different
+      programming languages.
+
+      <ul>
+        <li> Frontend written in Vue (Javasciprt) provides web frontend.</li>
+        <li> Backend written in Go provides api server.</li>
+        <li> Diagrams witten in Python (by mingrammer)</li>
+      </ul>
+    Because of Diagrams script actually a Python script. This app use
+    gVisor, an application kernel for containers
+    to run it in safe environment.
+    </p>
+
+    <p>
+    It also provide fuzzy search page of Nodes in Diagrams.
+    </p>
   </div>
 </template>
