fc441ce6dced266e6681514dfa3e5fcc21cb7b52
Homin Lee <ff4500@gmail.com>
2022-09-23 19:40:34 +0900

    url prefix working with 'yarn serve'

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Nodes from '../views/Nodes.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -20,11 +21,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/*',
+    component: NotFound,
   }
 ]
 
 const router = createRouter({
-  base: '/dgsb',
+  // base: '/dgsb',
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
