ef13c04c056fd288b20c121147a62bacd7dbb13a
Homin Lee <ff4500@gmail.com>
2023-01-24 01:25:37 +0900

    fix deploy script

--- push2gke.sh
+++ push2gke.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-git tag -a $1 -m "add tag for $1"
-git push --tags
+set -e
+
+rm -rf node_modules
 
 IMAGE_TAG=gcr.io/homin-dev/diagrams_fe
 IMAGE_NAME=$IMAGE_TAG:$1
@@ -11,3 +12,7 @@ docker push $IMAGE_NAME
 
 docker tag $IMAGE_NAME $IMAGE_NAME_LATEST
 docker push $IMAGE_NAME_LATEST
+
+git tag -a $1 -m "add tag for $1"
+git push --tags
+
